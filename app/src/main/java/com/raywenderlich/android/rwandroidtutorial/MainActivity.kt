4731c7cf31d6604553bfe539adb6a211c27622e2
Myrick Chow <myrickchow@gmail.com>
2021-06-16 17:56:41 +0800

    Add UI related codes

--- app/src/main/java/com/raywenderlich/android/rwandroidtutorial/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/rwandroidtutorial/MainActivity.kt
@@ -34,8 +34,15 @@
 
 package com.raywenderlich.android.runtracking
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -53,6 +60,15 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
   // Location & Map
   private lateinit var mMap: GoogleMap
 
+  // SharedPreferences
+  companion object {
+    private const val KEY_SHARED_PREFERENCE = "com.rwRunTrackingApp.sharedPreferences"
+    private const val KEY_IS_TRACKING = "com.rwRunTrackingApp.isTracking"
+  }
+  private var isTracking: Boolean
+    get() = this.getSharedPreferences(KEY_SHARED_PREFERENCE, Context.MODE_PRIVATE).getBoolean(KEY_IS_TRACKING, false)
+    set(value) = this.getSharedPreferences(KEY_SHARED_PREFERENCE, Context.MODE_PRIVATE).edit().putBoolean(KEY_IS_TRACKING, value).apply()
+
   override fun onCreate(savedInstanceState: Bundle?) {
     // Switch to AppTheme for displaying the activity
     setTheme(R.style.AppTheme)
@@ -64,7 +80,69 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     // Obtain the SupportMapFragment and get notified when the map is ready to be used.
     val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
     mapFragment.getMapAsync(this)
+
+
+    // Set up button click events
+    binding.startButton.setOnClickListener {
+      // Clear the PolylineOptions from Google Map
+      mMap.clear()
+
+      // Update Start & End Button
+      isTracking = true
+      updateButtonStatus()
+
+      // Reset the display text
+      updateAllDisplayText(0, 0f)
+
+      startTracking()
+    }
+    binding.endButton.setOnClickListener { endButtonClicked() }
+
+    // Update layouts
+    updateButtonStatus()
+
+    if (isTracking) {
+      startTracking()
+    }
   }
+
+  // UI related codes
+  private fun updateButtonStatus() {
+    binding.startButton.isEnabled = !isTracking
+    binding.endButton.isEnabled = isTracking
+  }
+
+  private fun updateAllDisplayText(stepCount: Int, totalDistanceTravelled: Float) {
+    binding.numberOfStepTextView.text =  String.format("Step count: %d", stepCount)
+    binding.totalDistanceTextView.text = String.format("Total distance: %.2fm", totalDistanceTravelled)
+
+    val averagePace = if (stepCount != 0) totalDistanceTravelled / stepCount.toDouble() else 0.0
+    binding.averagePaceTextView.text = String.format("Average pace: %.2fm/ step", averagePace)
+  }
+
+  private fun endButtonClicked() {
+    AlertDialog.Builder(this)
+        .setTitle("Are you sure to stop tracking?")
+        .setPositiveButton("Confirm") { _, _ ->
+          isTracking = false
+          updateButtonStatus()
+          stopTracking()
+        }.setNegativeButton("Cancel") { _, _ ->
+        }
+        .create()
+        .show()
+  }
+
+  // Tracking
+  @SuppressLint("CheckResult")
+  private fun startTracking() {
+
+  }
+
+  private fun stopTracking() {
+    
+  }
+
   // Map related codes
   /**
    * Manipulates the map once available.
